50c6c6e4e4fbfb3e446c41b23a01aa260d41c8b8
Fabrizio Mazzoni <fabrizio@fsm.co.tz>
2025-08-13 20:34:47 +0200

    Claude update 2

--- templates/index.php
+++ templates/index.php
@@ -4,10 +4,6 @@ style('drivermanager', 'style');
 ?>
 
 <div id="app">
-    <div id="app-navigation">
-        <?php print_unescaped($this->inc('navigation/index')); ?>
-    </div>
-
     <div id="app-content">
         <div id="app-content-wrapper">
             <?php print_unescaped($this->inc('part.content')); ?>
