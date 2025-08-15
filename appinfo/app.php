54667674189df739fc7ef23033cfd726719d3f0d
fabrizio <fabrizio.mazzoni@merzariotz.com>
2025-08-15 11:46:13 +0300

    BG Job fixes

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,11 +1,17 @@
 <?php
+// Register the background job for expiry notifications
+\OC::$server->getJobList()->add(\OCA\DriverManager\BackgroundJob\ExpiryNotification::class);
+
+// Log that the job has been registered (for debugging)
+\OC::$server->getLogger()->debug('DriverManager: ExpiryNotification job registered', ['app' => 'drivermanager']);
+
 // Register the app in navigation
 \OC::$server->getNavigationManager()->add(function () {
     return [
         'id' => 'drivermanager',
-        'order' => 10,
+        'order' => 10, 
         'href' => \OC::$server->getURLGenerator()->linkToRoute('drivermanager.page.index'),
         'icon' => \OC::$server->getURLGenerator()->imagePath('drivermanager', 'app.svg'),
         'name' => 'Driver Manager',
-    ];
+    ];  
 });
